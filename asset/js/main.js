39b290879e9cd7be4809288d36f7111a3bb17fd0
marif670 <arif670@gmail.com>
2025-10-30 13:32:03 +0500

    Add files via upload

---
+++ asset/js/main.js
@@ -0,0 +1,3 @@
+// small helpers
+document.getElementById && (document.getElementById('yr') && (document.getElementById('yr').textContent = new Date().getFullYear()));
+document.getElementById && (document.getElementById('yr2') && (document.getElementById('yr2').textContent = new Date().getFullYear()));
